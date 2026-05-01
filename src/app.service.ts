a0e26298fdb1c5431e3413af36d1f1b5a6823dd2
Awesome6192 <christianchibuike14@gmail.com>
2026-05-01 14:10:46 +0100

    chore: update root endpoint message to be more relevant

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Welcome to the Card Validator API. Use /api/v1/validate-card to validate card numbers.';
   }
 }
